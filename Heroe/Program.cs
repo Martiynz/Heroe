602f73c6e2346ee4c894da219b2d24149d46f855
Martiynz <143968800+Martiynz@users.noreply.github.com>
2024-10-18 11:13:21 -0600

    Metodo imprimir

--- Heroe/Program.cs
+++ Heroe/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Heroe;
+
+SuperHeroe superHeroe = new SuperHeroe();
+superHeroe.Nombre = "Super Man";
+superHeroe.IdentidadSecreta = "Clark Kent";
+superHeroe.Ciudad = "Metropolis";
+superHeroe.PuedeVolar = true;
+superHeroe.Imprimir();
+
+SuperHeroe superHeroe1 = new SuperHeroe();
+superHeroe1.Nombre = "Batman";
+superHeroe1.IdentidadSecreta = "Brus Wei";
+superHeroe1.Ciudad = "Gotham";
+superHeroe1.PuedeVolar = false;
+superHeroe1.Imprimir();
+
+SuperHeroe superHeroe2 = new SuperHeroe();
+superHeroe2.Nombre = "Flash";
+superHeroe2.IdentidadSecreta = "Barry";
+superHeroe2.Ciudad = "Central";
+superHeroe2.PuedeVolar= false;
+superHeroe2.Imprimir();
+
+
+
---
+++ Heroe/SuperHeroe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroe
+{
+    public class SuperHeroe
+    {
+        public string Nombre { get; set; }
+        public string IdentidadSecreta { get; set; }
+        public string Ciudad {  get; set; }
+        public Boolean PuedeVolar { get; set; }
+
+        public void Imprimir() 
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
+            Console.WriteLine($"Ciudad: {Ciudad}");
+            Console.WriteLine($"Puede Volar: {PuedeVolar}");
+        }
+    }
+}
