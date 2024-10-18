aca2a41fc4b288ba1df6bb8ac063bc484af979f7
Martiynz <143968800+Martiynz@users.noreply.github.com>
2024-10-18 11:45:39 -0600

    Información Completa

--- Heroe/Program.cs
+++ Heroe/Program.cs
@@ -6,6 +6,9 @@ superHeroe.Nombre = "Super Man";
 superHeroe.IdentidadSecreta = "Clark Kent";
 superHeroe.Ciudad = "Metropolis";
 superHeroe.PuedeVolar = true;
+superHeroe.super.Nombre = "Fuerza, volar";
+superHeroe.super.Descripcion = "Alien";
+superHeroe.super.Nivel = 9;
 superHeroe.Imprimir();
 
 SuperHeroe superHeroe1 = new SuperHeroe();
@@ -13,6 +16,9 @@ superHeroe1.Nombre = "Batman";
 superHeroe1.IdentidadSecreta = "Brus Wei";
 superHeroe1.Ciudad = "Gotham";
 superHeroe1.PuedeVolar = false;
+superHeroe1.super.Nombre = "No posee";
+superHeroe1.super.Descripcion = "La tecnologia hace que combata el grimen";
+superHeroe1.super.Nivel = 10;
 superHeroe1.Imprimir();
 
 SuperHeroe superHeroe2 = new SuperHeroe();
@@ -20,6 +26,9 @@ superHeroe2.Nombre = "Flash";
 superHeroe2.IdentidadSecreta = "Barry";
 superHeroe2.Ciudad = "Central";
 superHeroe2.PuedeVolar= false;
+superHeroe2.super.Nombre = "Velocidad";
+superHeroe2.super.Descripcion = "Su poder viene de un rayo que lo toco";
+superHeroe2.super.Nivel = 7;
 superHeroe2.Imprimir();
 
 
--- Heroe/SuperHeroe.cs
+++ Heroe/SuperHeroe.cs
@@ -13,12 +13,22 @@ namespace Heroe
         public string Ciudad {  get; set; }
         public Boolean PuedeVolar { get; set; }
 
+        public SuperPoder super;
+
+        public SuperHeroe()
+        {
+            super = new SuperPoder();
+        }
+
         public void Imprimir() 
         {
             Console.WriteLine($"Nombre: {Nombre}");
             Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
             Console.WriteLine($"Ciudad: {Ciudad}");
             Console.WriteLine($"Puede Volar: {PuedeVolar}");
+            Console.WriteLine($"Super poder: {super.Nombre}");
+            Console.WriteLine($"Descripción: {super.Descripcion}");
+            Console.WriteLine($"Nivel: {super.Nivel}");
         }
     }
 }
